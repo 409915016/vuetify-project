0ab98a672fa8697390ceeea39d3c4fb6dde3bf3b
Mather <409915016@qq.com>
2017-07-24 14:04:58 +0800

    * Components - Carousel.

--- src/App.vue
+++ src/App.vue
@@ -20,22 +20,21 @@
         <router-link to="/" tag="span" style="cursor: pointer;">DevMeetup</router-link>
       </v-toolbar-title>
       <v-toolbar-side-icon
-        @click.native.stop="sideNav = !sideNav"
+        @click.stop="sideNav = !sideNav"
         class="hidden-sm-and-up"
       ></v-toolbar-side-icon>
       <v-spacer></v-spacer>
-      <v-toolbar-item class="hidden-xs-only">
+      <v-toolbar-items class="hidden-xs-only">
         <v-btn
           flat
           v-for="item in menuItems"
           :key="item.title"
-          router
           :to="item.link"
         >
           <v-icon left dark>{{ item.icon}}</v-icon>
           {{ item.title }}
         </v-btn>
-      </v-toolbar-item>
+      </v-toolbar-items>
     </v-toolbar>
     <main>
       <router-view></router-view>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,63 @@
 <template>
-  <div>
-    <p>
-      The Home Page
-    </p>
-  </div>
+  <v-container>
+    <v-layout row wrap >
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item v-for="meetup in meetups" :src="meetup.imageUrl" :key="meetup.id">
+            <div class="title">{{ meetup.title }}</div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 sm12 class="text-xs-center ">
+        <p>Join our awesome meetups!</p>
+
+      </v-flex>
+
+    </v-layout>
+
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        meetups: [
+          {
+            imageUrl: 'http://att.bbs.duowan.com/forum/201608/26/043824v0tax8xt8tqhv7bl.jpg',
+            id: '1',
+            title: 'Meetup in New York'
+          },
+          {
+            imageUrl: 'http://pic1.win4000.com/wallpaper/b/589d620792a84.jpg',
+            id: '2',
+            title: 'Meetup in 2'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0, 0, 0, .5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
