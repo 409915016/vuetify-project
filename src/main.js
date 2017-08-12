ce70abe9ed50574de386f6a25aabaf8675dee9f2
Mather <409915016@qq.com>
2017-08-13 00:24:30 +0800

    + Adding Firebase Authentication .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -12,5 +13,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC6OxrvVxbK5Sh6V9beF5yZdtelIw8SN60',
+      authDomain: 'vue-test-e7efb.firebaseapp.com',
+      databaseURL: 'https://vue-test-e7efb.firebaseio.com',
+      projectId: 'vue-test-e7efb',
+      storageBucket: ''
+    })
+  }
 })
